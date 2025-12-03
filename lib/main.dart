64d1af0d2770138446b431a8794d9edcda21a72f
OTAKUWeBer <137557891+OTAKUWeBer@users.noreply.github.com>
2025-12-03 23:45:45 +0600

    Fix: assests and env

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'services/theme_service.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+  
   runApp(const QuizApp());
 }
 
